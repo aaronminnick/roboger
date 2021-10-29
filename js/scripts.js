b38f09c4fed06d37a2bd2c2e471f780edc984253
Aaron Minnick <abminnick@gmail.com>
2021-10-29 09:00:37 -0700

    pass test for non-number input

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@
 
 //Business Logic
 function neighbor(number) {
+  if (typeof number !== 'number') {
+    return null;
+  }
+
   let returnArray = [];
   for (let i = 0; i <= number; i++) {
     returnArray.push(i);
