1a7b8c554292bf5426d31e03e4f019e7837e5b9d
Aaron Minnick <abminnick@gmail.com>
2021-10-29 08:54:09 -0700

    add code to pass first couple tests

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Helper Functions
+
+//Business Logic
+function neighbor(number) {
+  let returnArray = [];
+  for (let i = 0; i <= number; i++) {
+    returnArray.push(i);
+  }
+  return returnArray;
+}
+
+//UI Logic
