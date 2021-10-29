5462792446b412a8deb3aee384906435773e45fa
Aaron Minnick <abminnick@gmail.com>
2021-10-29 09:14:37 -0700

    pass test for NaN or Infinity input

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 //Business Logic
 function neighbor(number) {
-  if (typeof number !== 'number') {
+  if (typeof number !== 'number' || number !== number || number === Infinity) {
     return null;
   }
 
